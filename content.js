b92e448fb9f092003afdfcafaf89f55614fb23f5
hsxo <hitesh.solanky@trilogy.com>
2020-06-05 18:29:42 +0530

    Major design and feature update

--- content.js
+++ content.js
@@ -1,21 +1,56 @@
-chrome.runtime.onMessage.addListener((searchText)=> {
-   let text1=searchText["scenario"]
-   let text2=searchText["text"]
-   
-   let pureScenario = text1.replace(/\n/g, " ").replace(/"/g,"").replace(/ /g,"").replace(/ /g,"");
-   let pureText = text2.replace(/\n/g, " ").replace(/"/g,"").replace(/ /g,"").replace(/ /g,"");
+var CONFIG ={
+   msgIdentifier : "hsxoe2e",
+   jiraDescriptionBoxId : "descriptionmodule",
+   jiraTinyMCEId: "tinymce",
+   enVDSURLMatcher: "https://confluence.devfactory.com/"
+}
 
-   var divs = document.getElementsByTagName('td')
-   for (var i = 0; i < divs.length; i++) {
-      divs[i].style.backgroundColor = "white";  
-      var pureScenarioSearch = divs[i].innerText.replace(/\n/g, " ").replace(/"/g,"").replace(/ /g,"").replace(/ /g,"").indexOf("cenario:"+pureScenario)
-      var pureTextSearch = divs[i].innerText.replace(/\n/g, " ").replace(/"/g,"").replace(/ /g,"").replace(/ /g,"").indexOf(pureText)
-      if (pureScenarioSearch > -1 ) {
-               if(pureTextSearch <0 &&  divs[i+1].innerText.replace(/\n/g, " ").replace(/"/g,"").replace(/ /g,"").replace(/ /g,"").indexOf(pureText)){
-                  i=i+1
-               }
-                divs[i].scrollIntoView()
-                divs[i].style.backgroundColor = "yellow";
+chrome.runtime.onMessage.addListener((message, sender, response)=> {
+   // do not do anything if message is not from extension
+   if(! message.hasOwnProperty(CONFIG.msgIdentifier)){
+      return;
+   }
+   message = message[CONFIG.msgIdentifier]
+   
+   if(message.hasOwnProperty("scenario")){
+      let text1=message["scenario"]
+      let text2=message["text"]
+      
+      let pureScenario = text1.replace(/\n/g, " ").replace(/"/g,"").replace(/ /g,"").replace(/ /g,"");
+      let pureText = text2.replace(/\n/g, " ").replace(/"/g,"").replace(/ /g,"").replace(/ /g,"");
+
+      var divs = document.querySelectorAll('#descriptionmodule td')
+      for (var i = 0; i < divs.length; i++) {
+         divs[i].style.backgroundColor = "white";  
+         var pureScenarioSearch = divs[i].innerText.replace(/\n/g, " ").replace(/"/g,"").replace(/ /g,"").replace(/ /g,"").indexOf("cenario:"+pureScenario)
+         var pureTextSearch = divs[i].innerText.replace(/\n/g, " ").replace(/"/g,"").replace(/ /g,"").replace(/ /g,"").indexOf(pureText)
+         if (pureScenarioSearch > -1 ) {
+                  if(pureTextSearch <0 &&  divs[i+1].innerText.replace(/\n/g, " ").replace(/"/g,"").replace(/ /g,"").replace(/ /g,"").indexOf(pureText)){
+                     i=i+1
+                  }
+                  divs[i].scrollIntoView()
+                  divs[i].style.backgroundColor = "yellow";
+         }
+      }
+   }
+
+   else if(message.hasOwnProperty("returnEnvDS")){
+      let envDS = null;
+      let toSearchInDiv = null;
+      if(document.getElementById(CONFIG.jiraDescriptionBoxId)){
+         toSearchInDiv = document.getElementById(CONFIG.jiraDescriptionBoxId)
+      }
+      else if(document.getElementById(CONFIG.jiraTinyMCEId)){
+         toSearchInDiv = document.getElementById(CONFIG.jiraTinyMCEId);
+      }
+      if(toSearchInDiv!=null){
+         envDS = toSearchInDiv.querySelector(`a[href^='${CONFIG.enVDSURLMatcher}']`)
+         console.log(envDS.href)
+         response(envDS.href)
+      }
+      else{
+         console.log("envDS not found")
+         response(null)
       }
    }
    });
--- popup.js
+++ popup.js
@@ -1,13 +1,115 @@
+CONFIG={
+  msgIdentifier : "hsxoe2e",
+  ticketValidatorEndPoint: "http://private.central-eks.aureacentral.com/pca-qe/api/review/",
+  envDSValidatorEndPoint: "http://private.central-eks.aureacentral.com/pca-qe/api/ticketservice/envDs/yaml",
+  EnvStatus : {
+    SELECTORS_NOT_SET : "EnvDS not found. You can set envDS manually",
+    ENV_FOUND : "An Environemnt was found",
+    LOADING_SELECTORS : "Loading Selectors",
+    SELECTORS_LOADED: "IntelliSelectors Activated from"
+  },
+  enVDSURLMatcher: "https://confluence.devfactory.com/",
+  INTELLI_SELECTORS_KEY : "IntelliSelectors",
+  timeout: 60000
+}
+var CURRENT_INTELLI_SELECTORS_LIST = null
+
+const loadIntelliSelectors = (url, topText, bottomText) => {
+  let obj = getFromLocalStorage(url)
+  let magicselectors=parseSelectors(obj["resp"])
+  let no__of_selectors = Object.keys(magicselectors).length
+  let date = new Date().toLocaleString();
+  let storage_selectors={url:url, date: date,  selectors: magicselectors}
+  localStorage.setItem(CONFIG.INTELLI_SELECTORS_KEY, JSON.stringify(storage_selectors))
+  CURRENT_INTELLI_SELECTORS_LIST = magicselectors
+  
+  // Show Status
+  let path = url.replace(CONFIG.enVDSURLMatcher, "")
+  $("#env-status-show").html(`(${no__of_selectors}) ${topText} <br> ${path} <br> ${date} <br>${bottomText}`)
+}
+
+const renderEnvDS = envds => {
+  if(!envds){
+    return;
+  }
+  console.log(envds)
+  let path = envds.replace(CONFIG.enVDSURLMatcher, "")
+  $("#env-url-input").val(envds)
+  let storedIntelliSelectors = getFromLocalStorage(CONFIG.INTELLI_SELECTORS_KEY)
+  
+  let do_we_have_envds = getFromLocalStorage(envds)
+  if(do_we_have_envds){
+    loadIntelliSelectors(envds, "Intelliselectors for environment loaded from", "Click to load recent one")   
+  }else{
+    $("#env-status-show").html(`${CONFIG.EnvStatus.ENV_FOUND} <br> ${path}`)
+  }
+  
+}
 
 document.addEventListener('DOMContentLoaded', function() {
-    var checkPageButton = document.getElementById('checkPage');
-    var storageFetchButton = document.getElementById('past-fetch');
-    var switchButtons = document.getElementsByClassName('switches');
-    
+  chrome.tabs.query({active: true,currentWindow: true},(tabs) => {
+    let message={}
+    message[CONFIG.msgIdentifier] = {returnEnvDS:""};
+    chrome.tabs.sendMessage(tabs[0].id, message, renderEnvDS);
+  });
+
+  $("#env-status-show").click(function(){
+    $("#env-show-hide").toggle();
+  });
+
+  let loadENVButton = document.getElementById('load-env-button');
+  var checkPageButton = document.getElementById('checkPage');
+  var storageFetchButton = document.getElementById('past-fetch');
+  var switchButtons = document.getElementsByClassName('switches');
+  
+  
+  loadENVButton.addEventListener('click', function() {    
+      chrome.tabs.getSelected(null, function(tab) {
+        let url =document.getElementById("env-url-input").value;
+        if(!url || url.length < 1){
+          url = tab.url;
+        }
+        let getENVURL = CONFIG.envDSValidatorEndPoint+`?url=${url}`
+        var xhr = new XMLHttpRequest();
+        xhr.onreadystatechange = function() {
+            if (xhr.readyState == 4 && xhr.status == 200) {
+              try{
+                console.log(xhr.responseText)
+                let nativeObject = YAML.parse(xhr.responseText)
+                console.log("\n=====\n\n")
+                console.log(nativeObject)
+                let date =  new Date().toLocaleString()
+                addTOLocalSTorage("env",url,{date: date, resp:nativeObject})
+                loadIntelliSelectors(url, CONFIG.EnvStatus.SELECTORS_LOADED, "")
+                $("#env-show-hide").hide()
+              }catch(e){
+                alert("Exception in fetching envDS")
+                $("#env-status-show").html("Exception in fetching envDS")
+                console.error(e)
+              }
+                
+            }
+            if( xhr.status > 299 && xhr.readyState == 4) {
+              console.log('Server Error: ' + xhr.statusText);
+              $("#env-status-show").html(`Exception in fetching envDS status ${xhr.status}`)
+          }
+        }
+        xhr.open("GET", getENVURL, true);
+        xhr.timeout = CONFIG.timeout; // Set timeout to 60 seconds (60000 milliseconds)
+        xhr.ontimeout = function () { 
+                    console.log('Request Timed out 60 secs');
+                    $("#env-status-show").html(`EnvDS Fetch Timedout`)
+                }
+        xhr.setRequestHeader("Content-Type", "application/json");
+        xhr.send();
+        $("#env-status-show").html(`${CONFIG.EnvStatus.LOADING_SELECTORS}`)
+    });
+  })
+
+
     storageFetchButton.addEventListener('click', function() {
       chrome.tabs.getSelected(null, function(tab) {
-          let url = tab.url;
-          let ticketKey = url.substring(url.lastIndexOf('/') + 1)
+          let ticketKey = getTicketKeyFromURL(tab.url)
           let storage=localStorage.getItem(ticketKey);
           if(storage){
             storage = JSON.parse(storage)
@@ -22,13 +124,12 @@ document.addEventListener('DOMContentLoaded', function() {
     });
 
     chrome.tabs.getSelected(null, function(tab) {
-        let url = tab.url;
-        let ticketKey = url.substring(url.lastIndexOf('/') + 1)
-
+      let ticketKey = getTicketKeyFromURL(tab.url)
       let storage=localStorage.getItem(ticketKey);
       if(storage){
           storage = JSON.parse(storage)
           $("#past-fetch").show();
+          $("#past-fetch").click();
           let date = new Date(storage["date"])
           date= date.toLocaleString()
           $("#check-info").html(`Stored last validation for <b>${ticketKey}</b> on <b>${date}</b>`)
@@ -60,10 +161,9 @@ document.addEventListener('DOMContentLoaded', function() {
     checkPageButton.addEventListener('click', function() {
 
       chrome.tabs.getSelected(null, function(tab) {
-        let url = tab.url;
-        let ticketKey = url.substring(url.lastIndexOf('/') + 1)
+        let ticketKey = getTicketKeyFromURL(tab.url)
         $("#check-info").html(`fetching validation for <b>${ticketKey}</b> ....`)
-        let validationURL = "http://private.central-eks.aureacentral.com/pca-qe/api/review/"+ticketKey
+        let validationURL = CONFIG.ticketValidatorEndPoint+ticketKey
         
 
         var xhr = new XMLHttpRequest();
@@ -76,8 +176,7 @@ document.addEventListener('DOMContentLoaded', function() {
                 let now = new Date()
                 let date= now.toLocaleString()
                 let storage = {resp:resp,date:now};
-                localStorage.setItem(ticketKey, JSON.stringify(storage));
-                
+                addTOLocalSTorage("e2e",ticketKey, storage);                
                 document.getElementById("loader-div").classList.remove("loading")
                 $("#check-info").html(`fetched validation for <b>${ticketKey}</b> on <b>${date}</b>`)
                 
@@ -89,7 +188,7 @@ document.addEventListener('DOMContentLoaded', function() {
           }
         }
         xhr.open("GET", validationURL, true);
-        xhr.timeout = 60000; // Set timeout to 60 seconds (60000 milliseconds)
+        xhr.timeout = CONFIG.timeout; // Set timeout to 60 seconds (60000 milliseconds)
         xhr.ontimeout = function () { 
                     console.log('Request Timed out 60 secs');
                     document.getElementById("loader-div").classList.remove("loading")
@@ -106,13 +205,16 @@ document.addEventListener('DOMContentLoaded', function() {
     let response = getPassAndFails(data)
     let passes = response["passes"]
     let fails = response["fails"]
+    let others = response["others"]
     
     $("#defaultOpen").html(`Fail (${fails.length})`);
     $("#passButton").html(`Pass (${passes.length})`);
+    $("#otherButton").html(`Other (${others.length})`);
 
 
     $("#failResponse").html(getHTMLFromArray(fails, "fail"))
     $("#passResponse").html(getHTMLFromArray(passes, "pass"))
+    $("#otherResponse").html(getHTMLFromArray(others, "other"))
 
     if(fails.length>0){
         $("#defaultOpen").click();
@@ -123,17 +225,18 @@ document.addEventListener('DOMContentLoaded', function() {
    console.log($(".show-me-desc"))
     $(".show-me-desc").click(function(){
       var innerHTML = this.parentNode.innerText;
-      let text= innerHTML.substring(innerHTML.indexOf("/")+1, innerHTML.indexOf("--")-2).trim()
+      let text= innerHTML.substring(innerHTML.indexOf("/")+1, innerHTML.indexOf("--")-1).trim()
       let scenario = innerHTML.split("/")[0]
       let searchText={text:text,scenario:scenario}
-      console.log(searchText)
+      let message={hsxoe2e:searchText}
+      console.log(message)
       
       const params = {
         active: true,
         currentWindow: true
       }
       chrome.tabs.query(params,(tabs) => {
-        chrome.tabs.sendMessage(tabs[0].id, searchText);
+        chrome.tabs.sendMessage(tabs[0].id, message);
     }); 
     })
   }
@@ -153,10 +256,14 @@ document.addEventListener('DOMContentLoaded', function() {
         html= html + ` <label class="tab-label" for="chck-${type}-${i}">${object.title} (${object.assertions.length})</label>`;
         for (let assertion of object.assertions){
           let showMe=""
+          let showType=""
+            if(type== "other"){
+              showType = assertion.result
+            }
           if(assertion.description.indexOf("/") > -1 && assertion.description.indexOf("--") >-1 && type === "fail"){
             showMe=`<button class="show-me-desc">show me</button>`;
           }
-            html= html + `<div class="tab-content">${escapeHtml(assertion.description)} ${showMe}</div>`;
+            html= html + `<div class="tab-content">${escapeHtml(assertion.description)} (${showType}) ${showMe}</div>`;
         }
         html= html + "</div>";
     }
@@ -166,6 +273,7 @@ document.addEventListener('DOMContentLoaded', function() {
   const getPassAndFails = function (data){
     let passes=[]
     let fails=[]  
+    let others=[] 
     let results = data["result"];
       for (let result of results){
           category = 1;
@@ -173,12 +281,14 @@ document.addEventListener('DOMContentLoaded', function() {
           let assertions = result["assertions"]
           let pass_assertions = []
           let fail_assertions = []
+          let other_assertions = []
           for (let assertion of assertions){
             if(assertion["result"]=="FAIL"){
                 fail_assertions.push(assertion)
-            }
-            else{
+            }else if(assertion["result"]=="PASS"){
                 pass_assertions.push(assertion)
+            }else{
+                other_assertions.push(assertion)
             }
           }
           
@@ -190,10 +300,15 @@ document.addEventListener('DOMContentLoaded', function() {
             let finalResult = {title : title, assertions : pass_assertions}
             passes.push(finalResult)
           }
+          if(other_assertions.length > 0){
+            let finalResult = {title : title, assertions : other_assertions}
+            others.push(finalResult)
+          }
       }
       console.log(fails)
       console.log(passes)
-      return {passes:passes, fails:fails}
+      console.log(others)
+      return {passes:passes, fails:fails, others: others}
   }
 
 
@@ -201,4 +316,64 @@ document.addEventListener('DOMContentLoaded', function() {
   function escapeHtml(str) {
     str = str.replace(/</g, "##-open-##").replace(/>/g, "##-close-##");
     return str.replace(/##-open-##/g, " <b>").replace(/##-close-##/g, "</b> ");
+}
+function getRAWURL(url){
+  let nurl = url.split("?")[0]
+  nurl=nurl.split("#")[0];
+  return nurl
+}
+
+function addTOLocalSTorage(type, key, obj){
+  localStorage.setItem(key, JSON.stringify(obj));
+  let extStoredKeys=localStorage.getItem("extension-stored-keys");
+  if(!extStoredKeys){
+    extStoredKeys={e2e:{}, env:{}}
+  }
+  else{
+    extStoredKeys=JSON.parse(extStoredKeys)
+  }
+  extStoredKeys[type][key]=new Date()
+  localStorage.setItem("extension-stored-keys", JSON.stringify(extStoredKeys));
+}
+function getFromLocalStorage(key){
+  let obj = localStorage.getItem(key)
+  if(!obj || obj.length<1){
+    return null
+  }
+  else return JSON.parse(obj)
+}
+
+function renderENV(){
+  let envs= getALlENVS();
+  let html = "<ul>";
+  let intelliSelectors=`<button class="intelli-selectors">Load to Intelli-Selectors</button>`;
+  for (var key in envs) {
+    if (envs.hasOwnProperty(key)) {
+        html = html + `<li> ${key} -- ${envs[key]} <br> ${intelliSelectors}</li>`
+    }
+  }
+  html = html + "</ul>";
+  $("#available-envs").html(html)
+}
+
+function getTicketKeyFromURL(url){
+ let rawURL = getRAWURL(url)
+ let ticketKey = rawURL.substring(rawURL.lastIndexOf('/') + 1)
+ return ticketKey;
+}
+function parseSelectors(obj){
+  let theMagicSelectorObj={}
+  let selectors=obj["selectors"]
+  for (group of selectors){
+    let groupName = group["name"]
+    let finalSelectors = group["selectors"]
+    for (var key in finalSelectors) {
+      if (finalSelectors.hasOwnProperty(key)) {
+        let serachKey = key.toLowerCase().replace(/ /g, "");
+        let serachValue={xpath:finalSelectors[key], group: groupName, name: key}
+        theMagicSelectorObj[serachKey]=serachValue
+      }
+    }
+  }
+  return theMagicSelectorObj;
 }
