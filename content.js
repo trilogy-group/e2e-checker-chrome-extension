0582dce610ae1eab4f13d1ca3b9075d4d913ac76
hsxo <hitesh.solanky@trilogy.com>
2020-06-05 12:35:58 +0530

    search optimization

--- content.js
+++ content.js
@@ -1,14 +1,19 @@
 chrome.runtime.onMessage.addListener((searchText)=> {
    let text1=searchText["scenario"]
    let text2=searchText["text"]
-   debugger;
+   
+   let pureScenario = text1.replace(/\n/g, " ").replace(/"/g,"").replace(/ /g,"").replace(/ /g,"");
+   let pureText = text2.replace(/\n/g, " ").replace(/"/g,"").replace(/ /g,"").replace(/ /g,"");
+
    var divs = document.getElementsByTagName('td')
    for (var i = 0; i < divs.length; i++) {
       divs[i].style.backgroundColor = "white";  
-      var index1 = divs[i].innerText.indexOf(text1);
-      var index2 = divs[i].innerText.indexOf(text2)
-      if (index1 != -1) {
-               //elem.scrollIntoView()
+      var pureScenarioSearch = divs[i].innerText.replace(/\n/g, " ").replace(/"/g,"").replace(/ /g,"").replace(/ /g,"").indexOf("cenario:"+pureScenario)
+      var pureTextSearch = divs[i].innerText.replace(/\n/g, " ").replace(/"/g,"").replace(/ /g,"").replace(/ /g,"").indexOf(pureText)
+      if (pureScenarioSearch > -1 ) {
+               if(pureTextSearch <0 &&  divs[i+1].innerText.replace(/\n/g, " ").replace(/"/g,"").replace(/ /g,"").replace(/ /g,"").indexOf(pureText)){
+                  i=i+1
+               }
                 divs[i].scrollIntoView()
                 divs[i].style.backgroundColor = "yellow";
       }
